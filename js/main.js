de924b024a9753769c1c742be8a1a313bef938f3
Federico Commisso <fdcommisso@gmail.com>
2020-08-09 17:58:39 -0400

    update - add work samples

--- js/main.js
+++ js/main.js
@@ -16,4 +16,168 @@ window.onload = function () {
       header.classList.remove("sticky");
     }
   }
+
+  const WORK = {
+    path: 'img/work/',
+    name: 'work',
+    type: 'folder',
+    children:
+      [
+      {
+        path: 'img/work//A10089570_10_3.jpg',
+        name: 'A10089570_10_3.jpg',
+        type: 'file'
+      },
+      {
+        path: 'img/work//A10089570_11_3.jpg',
+        name: 'A10089570_11_3.jpg',
+        type: 'file'
+      },
+      {
+        path: 'img/work//A10089570_12_3.jpg',
+        name: 'A10089570_12_3.jpg',
+        type: 'file'
+      },
+      {
+        path: 'img/work//A10089570_13_3.jpg',
+        name: 'A10089570_13_3.jpg',
+        type: 'file'
+      },
+      {
+        path: 'img/work//A10089570_14_3.jpg',
+        name: 'A10089570_14_3.jpg',
+        type: 'file'
+      },
+      {
+        path: 'img/work//A10089570_15_3.jpg',
+        name: 'A10089570_15_3.jpg',
+        type: 'file'
+      },
+      {
+        path: 'img/work//A10089570_18_3.jpg',
+        name: 'A10089570_18_3.jpg',
+        type: 'file'
+      },
+      {
+        path: 'img/work//A10089570_19_3.jpg',
+        name: 'A10089570_19_3.jpg',
+        type: 'file'
+      },
+      {
+        path: 'img/work//A10089570_1_3.jpg',
+        name: 'A10089570_1_3.jpg',
+        type: 'file'
+      },
+      {
+        path: 'img/work//A10089570_20_3.jpg',
+        name: 'A10089570_20_3.jpg',
+        type: 'file'
+      },
+      {
+        path: 'img/work//A10089570_21_3.jpg',
+        name: 'A10089570_21_3.jpg',
+        type: 'file'
+      },
+      {
+        path: 'img/work//A10089570_23_3.jpg',
+        name: 'A10089570_23_3.jpg',
+        type: 'file'
+      },
+      {
+        path: 'img/work//A10089570_26_3.jpg',
+        name: 'A10089570_26_3.jpg',
+        type: 'file'
+      },
+      {
+        path: 'img/work//A10089570_27_3.jpg',
+        name: 'A10089570_27_3.jpg',
+        type: 'file'
+      },
+      {
+        path: 'img/work//A10089570_28_3.jpg',
+        name: 'A10089570_28_3.jpg',
+        type: 'file'
+      },
+      {
+        path: 'img/work//A10089570_29_3.jpg',
+        name: 'A10089570_29_3.jpg',
+        type: 'file'
+      },
+      {
+        path: 'img/work//A10089570_2_3.jpg',
+        name: 'A10089570_2_3.jpg',
+        type: 'file'
+      },
+      {
+        path: 'img/work//A10089570_3.jpg',
+        name: 'A10089570_3.jpg',
+        type: 'file'
+      },
+      {
+        path: 'img/work//A10089570_3_3.jpg',
+        name: 'A10089570_3_3.jpg',
+        type: 'file'
+      },
+      {
+        path: 'img/work//A10089570_5_3.jpg',
+        name: 'A10089570_5_3.jpg',
+        type: 'file'
+      },
+      {
+        path: 'img/work//A10089570_6_3.jpg',
+        name: 'A10089570_6_3.jpg',
+        type: 'file'
+      },
+      {
+        path: 'img/work//A10089570_7_3.jpg',
+        name: 'A10089570_7_3.jpg',
+        type: 'file'
+      },
+      {
+        path: 'img/work//A10089570_8_3.jpg',
+        name: 'A10089570_8_3.jpg',
+        type: 'file'
+      },
+      {
+        path: 'img/work//A10089570_9_3.jpg',
+        name: 'A10089570_9_3.jpg',
+        type: 'file'
+      }]
+  };
+
+  let html = `
+      <div class="gallery-section">
+        <div class="container">
+          <div class='row align-center'>
+            <h2>Recent Work</h2>
+
+            <div class="fotorama"
+              data-arrows="true"
+              data-click="true"
+              data-swipe="true"
+              data-link="file"
+              data-columns="0"
+              data-size="large"
+              data-nav="thumbs"
+              data-allowfullscreen="true"
+              data-transition="slide"
+              data-width="1200"
+              data-auto="false"
+              data-max-width="100%"
+              data-ratio="1.77777777778">
+      `;
+
+  WORK.children.forEach((img, i) => {
+    html += `<img src="${img.path}" alt="${img.name || img.path}" />\n`;
+  });
+
+  html += `
+          </div>
+        </div>
+      </div>
+    </div>
+    `;
+
+  $('#gallery').html(html);
+  $('.fotorama').fotorama();
 }
